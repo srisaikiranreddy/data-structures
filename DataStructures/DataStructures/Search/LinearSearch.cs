cd44574af4a6bf95ab8539461af8ce1baaf4e2d2
SaiKiran Palle <srisaikiranreddy@gmail.com>
2020-05-31 20:58:21 -0400

    adding search

---
+++ DataStructures/DataStructures/Search/LinearSearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace DataStructures.Search
+{
+    public class LinearSearch
+    {
+        public LinearSearch()
+        {
+        }
+
+        static int[] LinearSearchNumbers(int[] searchArray, int[] findNumbers)
+        {
+           int[] numberFound = new int[findNumbers.Length];
+           for (int number = 0; number < findNumbers.Length; number++)
+           {
+                for(int i = 0; i < searchArray.Length; i++)
+                {
+                    if (findNumbers[number] == searchArray[i])
+                    {
+                        numberFound[number] = findNumbers[number];
+                    }
+                }
+
+            }
+            return numberFound;
+        }
+    }
+}
