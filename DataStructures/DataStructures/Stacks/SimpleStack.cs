0f19ea69411552c6a18aba1fbc0401ca903319a2
SaiKiran Palle <srisaikiranreddy@gmail.com>
2020-06-03 21:44:17 -0400

    main

---
+++ DataStructures/DataStructures/Stacks/SimpleStack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace DataStructures.Stacks
+{
+    public class SimpleStack
+    {
+        public SimpleStack()
+        {
+            Stack obj = new Stack();
+
+        }
+
+        static void StackPush(Object n, Stack stackObj)
+        {
+            stackObj.Push(n);
+        }
+    }
+}
