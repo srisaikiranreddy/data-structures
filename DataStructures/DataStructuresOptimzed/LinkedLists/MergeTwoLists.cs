bd2470883119be0587857655d597699d4985a51f
SaiKiran Palle <srisaikiranreddy@gmail.com>
2021-11-23 23:40:42 -0500

    checking for ds

---
+++ DataStructures/DataStructuresOptimzed/LinkedLists/MergeTwoLists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace DataStructuresOptimzed.LinkedLists
+{
+    public class MergeTwoLists
+    {
+        public ListNode MergeTwoListss(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode();
+
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val <= l2.val)
+                {
+                    result.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    result.next = l2;
+                    l2 = l2.next;
+                }
+            }
+            result.next = (l1 != null) ? l1 : l2;
+            return result;
+        }
+    }
+}
---
+++ DataStructures/DataStructuresOptimzed/LinkedLists/ReverseKGroup.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DataStructuresOptimzed.LinkedLists
+{
+    public class ReverseKGroup
+    {
+        public ReverseKGroup()
+        {
+        }
+    }
+}
